de2f1eef70423f0f98ec2a27b2f056ee05a2b64a
Elias Karimi <e-karimi@hotmail.com>
2023-02-02 20:26:31 +0100

    initial commit

---
+++ controllers/image.js
@@ -0,0 +1,63 @@
+   
+const USER_ID = 'devhise';
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = '3db47d73f1af484b8e5a08ca4b415e07';
+const APP_ID = 'my-first-application';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
+
+
+const handleApiCall = (req, res) => {
+    const IMAGE_URL = req.body.input;
+    const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+     // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+  // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+  // this will default to the latest version_id
+
+  fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+  .then(response => response.json())
+  .then(data => res.json(data))
+  .catch(err => res.status(400).json(err, 'unable to work with API'))
+}        
+    
+
+const handleImage = (db) => (req, res) => {
+    const { id } = req.body;
+    
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json("unable to get entries"));
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      user : 'postgres',
+      password : 'test',
+      database : 'smartbrain'
+    }
+});
+
+const app = express();
+app.listen(3001, () => {
+    console.log("app is running on port 3001");
+})
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+
+// requests
+app.get('/', (req, res) => { res.send('success');});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt));
+app.get('/profile/:id', profile.handleProfileGet(db));
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
+
+
+
+
